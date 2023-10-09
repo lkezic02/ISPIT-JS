b71b6c578f9e48e1f876c1e4d4f23db43d401ddc
lkezic02 <lukakezic02@hotmail.com>
2023-10-09 20:14:44 +0200

    ispit zdk

---
+++ script.js
@@ -0,0 +1,107 @@
+var button = document.getElementById("search-button");
+var input = document.getElementById("search-input");
+var tbody = document.querySelector(".rezultati tbody");
+var spinner = document.querySelector(".spinner");
+var poruka = document.getElementById("poruka");
+
+input.addEventListener("keyup", function () {
+    updateResults();
+});
+
+button.addEventListener("click", () => {
+    var unos = input.value;
+
+    spinner.style.display = "block";
+    poruka.style.display = "none";
+
+    setTimeout(() => {
+        spinner.style.display = "none";
+
+        var data = [];
+
+        if (data.length > 0) {
+            tbody.innerHTML = "";
+            data.forEach((element) => {
+                const row = document.createElement("tr");
+
+                const imeSerije = document.createElement("td");
+                imeSerije.innerText = element.show.name;
+                row.appendChild(imeSerije);
+
+                const rating = document.createElement("td");
+                rating.innerText = element.show.rating.average || "Nema podataka";
+                if (element.show.rating.average === null) {
+                    rating.classList.add("no-data");
+                }
+                row.appendChild(rating);
+
+                const genres = document.createElement("td");
+                genres.innerText = element.show.genres.join(", ") || "Nema podataka";
+                if (element.show.genres === null) {
+                    genres.classList.add("no-data");
+                }
+                row.appendChild(genres);
+
+                const sazetak = document.createElement("td");
+                sazetak.innerText = element.show.summary || "Nema podataka";
+                if (element.show.summary === null) {
+                    sazetak.classList.add("no-data");
+                }
+                row.appendChild(sazetak);
+
+                tbody.appendChild(row);
+            });
+        } else {
+            poruka.style.display = "block";
+        }
+    }, 2000);
+});
+
+function updateResults() {
+    var unos = input.value;
+    const urlAPI = "https://api.tvmaze.com/search/shows?q=" + unos;
+
+    fetch(urlAPI)
+        .then((response) => response.json())
+        .then((data) => {
+            tbody.innerHTML = "";
+
+            if (data && data.length > 0) {
+                data.forEach((element) => {
+                    const row = document.createElement("tr");
+
+                    const imeSerije = document.createElement("td");
+                    imeSerije.innerText = element.show.name;
+                    row.appendChild(imeSerije);
+
+                    const rating = document.createElement("td");
+                    rating.innerText = element.show.rating.average || "Nema podataka";
+                    if (element.show.rating.average === null) {
+                        rating.classList.add("no-data");
+                    }
+                    row.appendChild(rating);
+
+                    const genres = document.createElement("td");
+                    genres.innerText = element.show.genres.join(", ") || "Nema podataka";
+                    if (element.show.genres === null) {
+                        genres.classList.add("no-data");
+                    }
+                    row.appendChild(genres);
+
+                    const sazetak = document.createElement("td");
+                    sazetak.innerText = element.show.summary || "Nema podataka";
+                    if (element.show.summary === null) {
+                        sazetak.classList.add("no-data");
+                    }
+                    row.appendChild(sazetak);
+
+                    tbody.appendChild(row);
+                });
+            } else {
+                poruka.style.display = "block";
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
